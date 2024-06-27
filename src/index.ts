41ebc3c675e7f67114dcb1ed9ce125618c595946
Dzakky Budiman <dzakkybudiman19@gmail.com>
2024-06-27 19:22:34 +0700

    Add user authentication endpoints

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,17 @@ app.post('/login', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+// Endpoint untuk register user
+app.post('/register', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk register user
+    res.send('User registered');
+});
+
+// Endpoint untuk login user
+app.post('/login', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk login user
+    res.send('User logged in');
+});
